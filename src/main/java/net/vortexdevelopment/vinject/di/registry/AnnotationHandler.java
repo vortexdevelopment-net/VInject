9feea74cb9ef846be6f9706b25d0d6e692d46880
ceze88 <dev.ceze@gmail.com>
2024-12-19 12:48:41 +0100

    Provide DependencyContainer for anntation handlers so they can create injected instances of the class

--- src/main/java/net/vortexdevelopment/vinject/di/registry/AnnotationHandler.java
+++ src/main/java/net/vortexdevelopment/vinject/di/registry/AnnotationHandler.java
@@ -1,5 +1,6 @@
 package net.vortexdevelopment.vinject.di.registry;
 
+import net.vortexdevelopment.vinject.di.DependencyContainer;
 import sun.misc.Unsafe;
 
 import java.lang.annotation.Annotation;
@@ -11,5 +12,5 @@ public abstract class AnnotationHandler {
 
     public abstract Class<? extends Annotation> getAnnotation();
 
-    public abstract void handle(Class<?> clazz);
+    public abstract void handle(Class<?> clazz, DependencyContainer dependencyContainer);
 }
