ee0deff43e9d56d02d415da3e76df1652e52bb90
ceze88 <dev.ceze@gmail.com>
2025-04-19 23:39:35 +0200

    Add javadocs

--- VInject-Framework/src/main/java/net/vortexdevelopment/vinject/database/repository/CrudRepository.java
+++ VInject-Framework/src/main/java/net/vortexdevelopment/vinject/database/repository/CrudRepository.java
@@ -2,7 +2,14 @@ package net.vortexdevelopment.vinject.database.repository;
 
 import java.util.Optional;
 
+/**
+ * Generic CRUD repository interface for basic CRUD operations.
+ *
+ * @param <T> the entity type
+ * @param <ID> the type of the entity's identifier
+ */
 public interface CrudRepository<T, ID> {
+
     <S extends T> S save(S entity);
 
     <S extends T> Iterable<S> saveAll(Iterable<S> entities);
